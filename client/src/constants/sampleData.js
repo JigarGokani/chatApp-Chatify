f87c28d2fa52c2ee9f80a61b9ca758350d0e06c1
Jigar-gokani <jigar.gokani@somaiya.edu>
2024-02-28 17:40:07 +0530

    search integration

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -15,3 +15,16 @@ export const sampleChats =[
         members:["1","2"]
     }
 ]
+
+export const sampleUsers =[{
+    avatar:["https://www.w3schools.com/howto/img_avatar.png"],
+    name:"JIgar Gokani",
+    _id:"1",
+   
+},
+{
+    avatar:["https://www.w3schools.com/howto/img_avatar.png"],
+    name:"Binit Kumar",
+    _id:"2",
+   
+}]
