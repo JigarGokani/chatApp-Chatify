f53227b3ecee7c36a6b3dc1dfeb5109576463c7d
Jigar-gokani <jigar.gokani@somaiya.edu>
2024-04-18 15:19:44 +0530

    cokkie set again

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,11 +3,10 @@ import jwt from "jsonwebtoken";
 import { ErrorHandler } from "../utils/utility.js";
 import { adminSecretKey } from "../app.js";
 import { CHATIFY_TOKEN } from "../constants/config.js";
-import { User } from "../models/user.js";
 
 
 const isAuthenticated = TryCatch((req, res, next) => {
-    const token = req.cookies[chatify-token];
+    const token = req.cookies[CHATIFY_TOKEN];
     if (!token)
       return next(new ErrorHandler("Please login to access this route", 401));
   
