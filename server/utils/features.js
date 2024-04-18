8f325e3ffe3304839e011d4edba873ae83b8e030
Jigar-gokani <jigar.gokani@somaiya.edu>
2024-04-18 16:31:07 +0530

    cookie changed

--- server/utils/features.js
+++ server/utils/features.js
@@ -6,10 +6,9 @@ import { getBase64, getSockets } from "../lib/helper.js";
 
 const cookieOptions = {
     maxAge:15 *24 * 60 * 60 *1000,
-    sameSite:"None",
+    sameSite:"Lax",
     httpOnly:true,
     secure:true,
-    Partitioned,
 }
 
 const connectDB = (url) =>{
